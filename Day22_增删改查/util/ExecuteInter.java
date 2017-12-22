b66228f566ba18f8c61c97162d3788ec70b8f907
VeryHairy <34090308+veryhairy@users.noreply.github.com>
2017-12-22 19:36:50 +0800

    e

--- Day22_增删改查/util/ExecuteInter.java
+++
@@ -1,9 +0,0 @@
-package com.lanou3g.util;
-
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
-
-public interface ExecuteInter {
-    Statement execute(Connection conn) throws SQLException;
-}
